64742a85b3b1466f414fc3953df932d4210a9628
David Merrick <david@david-merrick.com>
2020-02-17 16:16:32 -0800

    Fixed bugs with action

--- .github/actions/codacy/entrypoint.sh
+++ .github/actions/codacy/entrypoint.sh
@@ -1,7 +1,9 @@
+#!/bin/sh -l
+
 CODACY_TOKEN=$1
 REPORT_PATH=$2
 COMMIT_ID=$3
 
 apt-get update && apt-get install -y curl jq
 curl -LS -o codacy-coverage-reporter-assembly.jar "$(curl -LSs https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | endswith(".jar"))) | .[0].browser_download_url')"
-java -jar codacy-coverage-reporter-assembly.jar report -l Kotlin -r $REPORT_PATH --commit-uuid $COMMIT_ID -c $CODACY_TOKEN
+java -jar codacy-coverage-reporter-assembly.jar report -l Kotlin -r "$REPORT_PATH" --commit-uuid "$COMMIT_ID" -c "$CODACY_TOKEN"
