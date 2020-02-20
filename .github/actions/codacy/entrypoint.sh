69b56ebe8b6fab29a24124162fdb26b14d385a5f
David Merrick <david@david-merrick.com>
2020-02-20 07:53:27 -0800

    Moved params to env vars

--- .github/actions/codacy/entrypoint.sh
+++ .github/actions/codacy/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -l
 
-export CODACY_PROJECT_TOKEN=$1
-REPORT_PATH=$2
-COMMIT_ID=$3
-
 java -jar codacy-coverage-reporter-assembly.jar report -l Kotlin -r "$REPORT_PATH" --commit-uuid "$COMMIT_ID"
