e3c22cf0a07ea4aa2050700b863bf3d411d778f6
David Merrick <david@david-merrick.com>
2020-02-21 16:37:52 -0800

    Used absolute path for jar

--- .github/actions/codacy/entrypoint.sh
+++ .github/actions/codacy/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-java -jar codacy-coverage-reporter-assembly.jar report -l Kotlin -r "$REPORT_PATH" --commit-uuid "$COMMIT_ID"
+java -jar /bin/codacy-coverage-reporter-assembly.jar report -l Kotlin -r "$REPORT_PATH" --commit-uuid "$COMMIT_ID"
